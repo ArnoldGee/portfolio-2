2d2afed2c91ec3bdaac7de6376d9fcb197542004
Arnau Gómez <agomez1f@gmail.com>
2020-09-12 00:15:56 +0200

    📦Added AOS CDN and had fun dabbling with it

--- src/pages/presentation/presentation.component.jsx
+++ src/pages/presentation/presentation.component.jsx
@@ -4,8 +4,8 @@ import {PresentationTitle, CoverImage, Emphasis} from './presentation.styles'
 export default function Main() {
   return (
     <div>
-      <PresentationTitle>Boring?</PresentationTitle>
-      <Emphasis>Never Again!</Emphasis>
+      <PresentationTitle data-aos="fade-right" data-aos-duration="3000">Boring?</PresentationTitle>
+      <Emphasis >Never Again!</Emphasis>
       <CoverImage src={require('../../assets/IMG_1008 còpia.JPG')} />
     </div>
   )
